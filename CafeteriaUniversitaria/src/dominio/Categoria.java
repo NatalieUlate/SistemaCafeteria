4b4817d204b537206920811ee1afa5573b31e713
NataieUlate <ulaterojasnatalie@gmail.com>
2025-08-10 21:55:11 -0600

    Creacion de clase Categoria dentro del paquete dominio

---
+++ CafeteriaUniversitaria/src/dominio/Categoria.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dominio;
+
+/**
+ *
+ * @author Natalie Ulate Rojas
+ */
+public class Categoria {
+
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private boolean activa;
+
+    public Categoria() {
+    }
+
+    public Categoria(int id, String nombre, String descripcion, boolean activa) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.activa = activa;
+    }
+
+    // --- Getters y Setters ---
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isActiva() {
+        return activa;
+    }
+
+    public void setActiva(boolean activa) {
+        this.activa = activa;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return this.nombre;
+    }
+}
